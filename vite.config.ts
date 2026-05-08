d97b87535cc33b94715132bde1fe4a312932275a
Lais Gabrielle Lodi <gabriellelodi@gmail.com>
2026-05-08 16:44:55 -0600

    Revert "Add tests and improve test coverage"
    This reverts commit 371e5ed42dc09e92bd42c466415db6c2541d29ef.

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
   base: '/hebrew_calendar/',
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/test/setup.ts'],
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      include: ['src/**/*.{ts,tsx}'],
-      exclude: ['src/test/**', 'src/main.tsx', 'src/**/*.test.{ts,tsx}'],
-    },
-  },
 })
